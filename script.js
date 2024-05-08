74af096dcaf2da3c3432df60964afc5c27195b9b
eizw <hamzahamri2005@gmail.com>
2024-05-09 00:14:53 +0800

    Added editing buttons

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-$('.preview-btn').click(function(e){
-    addLay($(this).html());
-})
-$('.preview-btn').prop('disabled', true)
-
 let $currentBox = null;
 
 init()
 
 function init() {
     console.log('skibidi')
+
+    $('.preview-btn').click(function(e) {
+        addLay($(this).html());
+    })
+    $('.edit-btn').click(editLay)
+    $('.del-btn').click(delLay)
+
+    $('.preview-btn').prop('disabled', true)
+    disableEdit()
+    
     $('.preview-screen').click(selectBox)
     $('.preview-screen').append($('<div class="layout"></div>').click(selectBox))
 }
 
+
+
 function selectBox(e) {
     e.stopPropagation();
     $(this).attr('id', 'selected')
-    if (!$currentBox) $('.preview-btn').prop('disabled', false)
+    if (!$currentBox) {
+        $('.preview-btn').prop('disabled', false)
+
+    }
+    if (!$(this).hasClass('preview-screen')) {
+        enableEdit()
+    } else {
+        disableEdit()
+    }
+
     $('.layout').not(this).attr('id', '')
     $currentBox = $(this)
 }
@@ -34,4 +50,26 @@ function addLay(type) {
             $currentBox.append($(`<div class="layout ${type}-lay"></div>`).click(selectBox))
         }
     }
+}
+
+function editLay(e) {
+    
+}
+
+function delLay(e) {
+    if (confirm(`Delete layout?`)) {
+        $currentBox.remove();
+        $('.preview-btn').prop('disabled', true)
+        disableEdit();
+        $currentBox = null
+    }
+}
+
+function disableEdit() {
+    $('.edit-btn').prop('disabled', true)
+    $('.del-btn').prop('disabled', true)
+}
+function enableEdit() {
+    $('.edit-btn').prop('disabled', false)
+    $('.del-btn').prop('disabled', false)
 }
