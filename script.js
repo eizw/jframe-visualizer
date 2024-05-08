13228c65c4b9aa002dc00bcabe44f7ca5f2a24ef
eizw <hamzahamri2005@gmail.com>
2024-05-08 23:06:47 +0800

    Initialized box selection feature

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-$('.preview-btn').click(function(){
-    $(this).css('background-color', 'yellow')
-    $('.preview-btn').not(this).css('background-color', 'white')
+$('.preview-btn').click(function(e){
+    addLay($(this).html());
 })
 
-$
+let CURRENT_LAYOUT;
+
+init()
+
+function init() {
+    console.log('skibidi')
+    $('.preview-screen').click(function(e) {
+        e.stopPropagation();
+        $(this).attr('id', 'selected')
+        $('.layout').not(this).attr('id', '')
+    })
+    $('.preview-screen').append($('<div class="layout"></div>').click(function(e) {
+        e.stopPropagation();
+        $(this).attr('id', 'selected')
+        $('.layout').not(this).attr('id', '')
+    }))
+}
+
+function addLay(type) {
+    type = type.substring(0, type.indexOf('L')).toLowerCase()
+
+    if (type == 'grid') {
+
+    } else if (type == '') {
+
+    }
+}
